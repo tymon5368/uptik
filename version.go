117fbc5e2d7a11b3e208bc44b22b3adcfbbf5662
Tymon <tymon3568@gmail.com>
2026-09-13 08:42:57 +0700

    chore(release): bump version to v1.2.1

--- version.go
+++ version.go
@@ -3,7 +3,7 @@ package main
 // Build metadata injected at compile-time via -ldflags
 var (
 	// Version is the current semantic version of the UpTik application
-	Version = "1.2.0"
+	Version = "1.2.1"
 
 	// Commit is the git commit hash of the build
 	Commit = "none"
